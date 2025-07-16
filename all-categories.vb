796ccb3c9cbc44d9136432f8bb0d82796d56b9b2
Shubham Gajanan Uparkar <65241730+shubham31121999@users.noreply.github.com>
2025-07-16 13:11:01 +0530

    Done

---
+++ all-categories.vb
@@ -0,0 +1,149 @@
+(A) Regenerative Therapies
+
+1.) Restorative Medicine at Trupeak
+
+
+2.) Cancer Nutrition at Trupeak
+
+3.) Renal Nutrition
+
+4.) Hormone Optimisation therapy
+
+5.) Pain Management
+      
+6.) NAD+ Therapy
+
+7.) IV Therapy
+
+
+8.) Post-surgical Rehabilitation
+
+9.) Chronic Illness Management
+
+
+
+(B) Weight and Lean Muscle Management at Trupeak
+1.) Weight Optimisation tracking at Trupeak
+
+2.) Functional Medicine Consultation
+
+
+3.) MuscleSound Analysis
+
+4.) Inbody970 Bioelectrical Impedance Analysis
+
+
+5.) Q NRG Indirect Calorimetry
+
+6.) Sleep Cycle Analysis
+
+7.) Diet Management and Nutritional Counselling
+
+
+(C) Immunity and Longevity
+
+1.) Immune System Assessment and Analysis
+
+2.) Genetic Mapping
+
+3.) Gut Microbiome Mapping
+
+4.) Biological Age Mapping
+
+(D)Pain Management and Recovery
+
+1.) Chronic Back Ache Induced by Uric Acid
+   
+2.) Arthritis
+
+3.) Accident Recovery
+
+4.) Joint Pain Management
+
+5.) Sports Recovery
+
+6.) Cryotherapy
+
+7.) Localized Cryotherapy
+
+(E) Women-centric Health Interventions
+
+1.) PCOS and PCOD 
+
+2.) Menopause-Related Issues
+
+3.) Hormone Balancing
+
+4.) Uterine Removal Complications in Older Women
+
+5.) HPV Vaccines
+
+6.) UTIs (Urinary Tract Infections)
+
+7.) Osteoporosis
+
+(F)Beauty Aesthetics
+ 
+   (1)Hair
+
+   (2)Face
+
+   (3)Lasers 
+
+   (4)Intimate Health
+
+   (5)Injectable Therapies (PRP, Botox, etc.)
+
+   (6)Surgical References (Plastic)
+
+   (7)Aesthetic Gynaecology
+
+(G) Ergogenic medicine
+
+      (1) Sexual Health
+
+      (2) Fertility (Pre)
+
+      (3) Virility
+
+      (4) Athletic Boost
+
+      (5) Intimate Health
+
+      (6) Endurance Boost
+
+      (7) Sexually Transmitted Diseases (STDs)
+
+(H) . Mental Health and Counselling
+
+1.) Mental Health Peptides
+
+2.) Chronic Illness counselling and support (Cancer specifically)
+
+3.) Nutrition Management protocols
+
+4.) Psychologist sessions
+
+5.) Paediatric Counselling (ADHD and other issues)
+
+(I)Technology
+
+ (1)Data-driven Healthcare
+
+ (2)Data-driven tracking
+
+ (3)Analysing over 800 Biomarkers
+
+ (4)Monthly automated reporting
+
+(J)At Home
+
+1.) Supplement Store
+
+2.) At-home Testing
+
+3.) Peptides
+
+(K)Home
+
+(L)About Us
